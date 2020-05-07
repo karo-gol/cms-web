bf75b8c264cdf391f119fcc84b8222a29e945a7e
karo-gol <golba.karolina@gmail.com>
2020-05-08 01:25:41 +0200

    basic web and graphql configs

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { useQuery } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+
+import graphqlClient from "#root/graphql/graphqlClient";
+
+const query = gql`
+    {
+        users {
+            userId
+            login
+            email
+        }
+    }
+`;
+
+const Root = () => {    
+    const { data, loading } = useQuery(query);
+
+    if(loading) return "Loading...";
+
+    return (        
+        <div>
+            {data.users.map(user => (
+                <div>{`${user.userId} - ${user.login} - ${user.email}`}</div>
+            ))}            
+        </div>
+    );
+};
+
+render(   
+    <ApolloProvider client={graphqlClient}> 
+        <Root />
+    </ApolloProvider>,   
+    document.getElementById("app")
+);
